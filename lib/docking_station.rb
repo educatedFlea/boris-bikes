14441280ba12f7db4ace0945e97e7e048dd9194a
ZhengZiyao <Chloe@ZhengZiyaos-MacBook-Air.local>
2020-02-18 16:17:22 +0000

    private method "full?" and "empty?" added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,14 +7,24 @@ class DockingStation
   end 
   
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end 
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.count >= 20
+    fail 'Docking station is full' if full?
     @bikes << bike
     @bike = bike
   end 
+
+  private 
+  def full?
+    @bikes.count >= 20
+  end 
+
+  def empty?
+    @bikes.empty?
+  end 
+
 end 
 
